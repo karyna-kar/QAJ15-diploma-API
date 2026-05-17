e78676bd2c9fc9c6ebd88266d157d4829ad7acb3
Karina <karyna.karmaza@gmail.com>
2026-05-17 18:19:29 +0300

    add custome fixtures

---
+++ fixtures/custome-fixtures.ts
@@ -0,0 +1,35 @@
+import { test as baseTest } from '@playwright/test';
+import { RestfulController } from '../controllers/controller';
+import 'dotenv/config';
+
+interface ExtendedFicture {
+  restfulControllerAuthorizedUser: RestfulController;
+  restfulControllerNotAuthorizedUser: RestfulController;
+  restfulControllerInvalidXMasterKey: RestfulController;
+}
+
+export const test = baseTest.extend<ExtendedFicture>({
+  restfulControllerAuthorizedUser: async ({ playwright }, use) => {
+    const baseURL = 'https://api.jsonbin.io';
+    const authRequest = await playwright.request.newContext({
+      extraHTTPHeaders: { 'X-Master-Key': process.env.X_MASTER_KEY as string },
+    });
+    const controller = new RestfulController(authRequest, baseURL);
+    await use(controller);
+  },
+
+  restfulControllerNotAuthorizedUser: async ({ request }, use) => {
+    const baseURL = 'https://api.jsonbin.io';
+    const controller = new RestfulController(request, baseURL);
+    await use(controller);
+  },
+
+  restfulControllerInvalidXMasterKey: async ({ playwright }, use) => {
+    const baseURL = 'https://api.jsonbin.io';
+    const authRequest = await playwright.request.newContext({
+      extraHTTPHeaders: { 'X-Master-Key': 'Test' },
+    });
+    const controller = new RestfulController(authRequest, baseURL);
+    await use(controller);
+  },
+});

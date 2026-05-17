c7bb8c7309094ce0ea9434310b9800ee49822928
Karina <karyna.karmaza@gmail.com>
2026-05-17 18:19:38 +0300

    add controller

---
+++ controllers/controller.ts
@@ -0,0 +1,48 @@
+import { APIRequestContext } from '@playwright/test';
+import { RequestObject } from '../types/types';
+
+export class RestfulController {
+  private request: APIRequestContext;
+  private baseUrl: string;
+
+  constructor(request: APIRequestContext, baseUrl: string) {
+    this.request = request;
+    this.baseUrl = baseUrl + '/v3/b';
+  }
+
+  async getBinById(id: string) {
+    return this.request.get(`${this.baseUrl}/${id}`);
+  }
+
+  async deleteBin(id: string) {
+    return this.request.delete(`${this.baseUrl}/${id}`);
+  }
+
+  async createBin(binPayload: RequestObject | {}) {
+    return this.request.post(this.baseUrl, {
+      data: binPayload,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  async createBinWithoutHeader(binPayload: RequestObject | {}) {
+    return this.request.post(this.baseUrl, {
+      data: JSON.stringify(binPayload),
+      headers: {},
+    });
+  }
+
+  async updateBin(id: string, binPayload: RequestObject | {}) {
+    return this.request.put(`${this.baseUrl}/${id}`, {
+      data: binPayload,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  async updateWithoutHeader(id: string, binPayload: RequestObject | {}) {
+    return this.request.put(`${this.baseUrl}/${id}`, {
+      data: JSON.stringify(binPayload),
+      headers: {},
+    });
+  }
+}
